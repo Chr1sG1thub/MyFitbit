4335bfa01497da8bf7f7592d7ac2707b3d8bd2c2
Chr1sG1thub <140241398+Chr1sG1thub@users.noreply.github.com>
2026-03-16 23:02:02 +0100

    Update fitbit.js

--- api/fitbit.js
+++ api/fitbit.js
@@ -9,8 +9,8 @@ export default async function handler(req, res) {
     return;
   }
 
-//  const { endpoint = '/1/user/-/hrv/date/today.json', token } = req.query;
-   const { endpoint = '/1.2/user/-/sleep/date/2026-03-15.json', token } = req.query;
+  const { endpoint = '/1/user/-/hrv/date/today.json', token } = req.query;
+//   const { endpoint = '/1.2/user/-/sleep/date/2026-03-15.json', token } = req.query;
  
   if (!token) {
     res.status(400).json({ error: 'No token' });
